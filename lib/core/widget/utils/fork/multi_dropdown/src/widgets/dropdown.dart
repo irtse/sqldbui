221247fb919f5d652b4fc1e15c9247d8a55074c5
mr <morgane.roques@irt-saintexupery.com>
2026-01-16 17:22:52 +0100

    filter search

--- lib/core/widget/utils/fork/multi_dropdown/src/widgets/dropdown.dart
+++ lib/core/widget/utils/fork/multi_dropdown/src/widgets/dropdown.dart
@@ -276,17 +276,17 @@ class _SearchField extends StatelessWidget {
       });
     }
     
-    FocusNode focusNode = FocusNode();
-    focusNode.requestFocus();
-    
-    
-
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+                    searchCtrl[label]?.selection = TextSelection.collapsed(
+                      offset: searchCtrl[label]?.text.length ?? 0,
+                    );
+                  });    
     return Padding(
       padding: const EdgeInsets.all(8),
       child: Wrap( 
       alignment: WrapAlignment.center, 
       children: [ TextField(
-        focusNode: focusNode,
+        autofocus: true,
         controller: searchCtrl[label]!,
         decoration: InputDecoration(
           isDense: true,
@@ -302,13 +302,6 @@ class _SearchField extends StatelessWidget {
               if (searchCtrl[label]?.text == v) {
                 changeFunction!(searchCtrl[label]?.text ?? "");
                 onChanged(search[label] ??  []);
-                Future.delayed(Duration(microseconds: 500), () {
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    searchCtrl[label]?.selection = TextSelection.collapsed(
-                      offset: searchCtrl[label]?.text.length ?? 0,
-                    );
-                  });
-                });
               } 
             });
           }
