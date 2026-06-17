6ad4c558b3e92db214d5d3c3e0e73006bbba02d8
mr <morgane.roques@irt-saintexupery.com>
2026-06-17 13:04:43 +0200

    new view

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -304,17 +304,17 @@ const RESOURCES = {"assets/AssetManifest.bin": "dc9cb90fbad5bf18dc076273c01abdb2
 "canvaskit/chromium/canvaskit.js.symbols": "e2d09f0e434bc118bf67dae526737d07",
 "canvaskit/skwasm_heavy.js": "740d43a6b8240ef9e23eed8c48840da4",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
-"index.html": "bc0730873022a1a112f8c4da29453a1e",
-"/": "bc0730873022a1a112f8c4da29453a1e",
+"index.html": "f38d5650e105740587026850b082c59f",
+"/": "f38d5650e105740587026850b082c59f",
 "favicon.png": "7acd80e0ce19e8cfc8ab84e17a5609ce",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"main.dart.js": "362970a9654f1753b2888695cf4e5b5f",
+"main.dart.js": "f8b4a4b7a24255b4baf35e8b754e6576",
 "version.json": "7b68e01ad8004e7c8352f33bfe38649f",
-"flutter_bootstrap.js": "0000830487e679f70de00b818765b1b8"};
+"flutter_bootstrap.js": "9f8ceefafd6bde1a5a571c98e575c364"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
--- lib/core/widget/datagrid/widget/row.dart
+++ lib/core/widget/datagrid/widget/row.dart
@@ -76,19 +76,19 @@ class GridRowWidgetState extends State<GridRowWidget> {
           return Container();
         })]),
         if (widget.sharedTo.isNotEmpty)
-          Positioned(left: 45, top: 30, child: FutureBuilder(future: getOnFlow("shared to :"), builder: (a,s) {
+          Positioned(left: 45, top: 0, bottom: 0, child: Center(child: FutureBuilder(future: getOnFlow("shared to :"), builder: (a,s) {
             if(s.data != null) {
               return Tooltip( message: "${s.data} ${widget.sharedTo.join(",")}", child: Icon(Icons.share, color: Theme.of(context).primaryColor));
             }
             return Tooltip( message:  widget.sharedTo.join(","), child: Icon(Icons.share, color: Theme.of(context).primaryColor));
-          }) ),
+          }))),
         if (widget.sharedBy.isNotEmpty)
-           Positioned(left: 45, top: 30, child: FutureBuilder(future: getOnFlow("shared by :"), builder: (a,s) {
+           Positioned(left: 45, top: 0, bottom: 0, child: Center(child: FutureBuilder(future: getOnFlow("shared by :"), builder: (a,s) {
             if(s.data != null) {
               return Tooltip( message: "${s.data} ${widget.sharedBy.join(",")}", child: Icon(Icons.share, color: Colors.grey));
             }
             return Tooltip( message:  widget.sharedBy.join(","), child: Icon(Icons.share, color: Colors.grey));
-          }) )
+          })))
       ])
     );
   }
